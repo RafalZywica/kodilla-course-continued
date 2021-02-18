2222ea4e6b2e6aaba2935bf204af595555de6c0a
Rafał Żywica <rafalzywica2701@wp.pl>
2021-02-18 17:04:00 +0100

    First commit

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,15 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	public static void main(String[] args) {
+
+	}
+
+}
