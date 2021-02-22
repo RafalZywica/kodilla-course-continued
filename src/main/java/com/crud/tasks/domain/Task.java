1952f5e8f9fb3c3bb2363096736bd9c05c70764a
Rafał Żywica <rafalzywica2701@wp.pl>
2021-02-22 17:00:06 +0100

    Committing 19.2 task

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,13 +2,17 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
 public class Task {
 
     @Id
