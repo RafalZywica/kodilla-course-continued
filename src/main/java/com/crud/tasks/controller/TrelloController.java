bf5272de5172271fcc8f2948ba07e4dc68a079f1
Rafał Żywica <rafalzywica2701@wp.pl>
2021-06-08 16:22:49 +0200

    32.3

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -53,12 +53,12 @@ public class TrelloController {
         return trelloService.createTrelloCard(trelloCardDto);
     }*/
 
-    @GetMapping("getTrelloBoards")
+    @GetMapping("/boards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloFacade.fetchTrelloBoards();
     }
 
-    @PostMapping("createTrelloCard")
+    @PostMapping("/cards")
     public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloFacade.createCard(trelloCardDto);
     }
